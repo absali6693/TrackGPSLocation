82b4c1308b6d46f2625d4a384790d4e770be2dcd
Abbas ALi <absali6693@gmail.com>
2016-09-13 15:54:12 +0530

    Function call in a given time implemented

--- TrackGPSLocation/TrackerViewController.swift
+++ TrackGPSLocation/TrackerViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class TrackerViewController: UIViewController {
-
+    
+    @IBOutlet weak private var usernameTestField: UITextField!
+    @IBOutlet weak private var urlTestField: UITextField!
+    @IBOutlet weak private var frequencyTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,26 @@ class TrackerViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
+//MARK: - IBAction
 
+extension TrackerViewController {
+    @IBAction func sendButtonClicked(sender: AnyObject) {
+        callInTime(self.frequencyTextField.text!, url: self.urlTestField.text!)
+    }
+    
+}
+
+//MARK:- To delete
+
+extension TrackerViewController {
+    func callInTime(time: String, url: String) {
+        NSTimer.scheduledTimerWithTimeInterval(15.0, target: self, selector: #selector(TrackerViewController.callFunction), userInfo: nil, repeats: true)
+    }
+    
+    func callFunction() {
+        print("Abbas")
+    }
 }
 
