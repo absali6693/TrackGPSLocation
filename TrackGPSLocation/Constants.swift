5301df10fb28eb14fc96b63cba914455de191295
Abbas ALi <absali6693@gmail.com>
2016-09-13 20:43:58 +0530

    Sending data to server

---
+++ TrackGPSLocation/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  TrackGPSLocation
+//
+//  Created by GrepRuby1 on 13/09/16.
+//  Copyright © 2016 GrepRuby. All rights reserved.
+//
+
+import Foundation
+
+struct GlobalConstants {
+    static let dateFormat = "EEE, dd MMM yyy hh:mm:ss +zzzz"
+    static let latitude = "latitude"
+    static let longitiude = "longitiude"
+    static let speed = "speed"
+    static let altitude = "altitude"
+    static let course = "course"
+    static let timestamp = "timestamp"
+    static let username = "username"
+}
--- TrackGPSLocation/SendDataToServer.swift
+++ TrackGPSLocation/SendDataToServer.swift
@@ -10,24 +10,42 @@ import Foundation
 import CoreLocation
 
 class SendDataToServer: NSObject{
-
+    
     
     func sendData(username: String, url: String,locations: CLLocation) {
         let locValue:CLLocationCoordinate2D = locations.coordinate
-        print("locations = \(locValue.latitude) \(locValue.longitude)")
-        print(locations.speed)
-        print(locations.altitude)
-        print(locations.course)
-        print(locations.timestamp)
-
-        
+        let postEndpoint = url
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = GlobalConstants.dateFormat
+        let timestamp: String = formatter.stringFromDate(locations.timestamp)
+        let url = NSURL(string: postEndpoint)!
+        let session = NSURLSession.sharedSession()
+        let locationData : [String: AnyObject] = [GlobalConstants.latitude: locValue.latitude, GlobalConstants.longitiude: locValue.longitude, GlobalConstants.speed: locations.speed, GlobalConstants.altitude: locations.altitude, GlobalConstants.course: locations.course, GlobalConstants.timestamp: timestamp]
+        let postParams : [String: AnyObject] = [GlobalConstants.username: username, "device_type": "IOS", "location_data": locationData]
+        // Create the request
+        let request = NSMutableURLRequest(URL: url)
+        request.HTTPMethod = "POST"
+        request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        do {
+            request.HTTPBody = try NSJSONSerialization.dataWithJSONObject(postParams, options: NSJSONWritingOptions())
+            print(postParams)
+        } catch {
+            print("Error")
+        }
+        // Make the POST call and handle it in a completion handler
+        session.dataTaskWithRequest(request, completionHandler: { ( data: NSData?, response: NSURLResponse?, error: NSError?) -> Void in
+            // Make sure we get an OK response
+            guard let realResponse = response as? NSHTTPURLResponse where
+                realResponse.statusCode == 200 else {
+                    print("Not a 200 response")
+                    return
+            }
+            // Read the JSON
+            if let postString = NSString(data:data!, encoding: NSUTF8StringEncoding) as? String {
+                // Print what we got from the call
+                print("POST: " + postString)
+            }
+            
+        }).resume()
     }
-}
-
-//MARK:- Delegate for location manager
-
-extension SendDataToServer {
-    
-   
-    
 }
